2dd82e2f1285b85528e9b30b05b19c5fbb048dc2
putnam3d <putnam3d@gmail.com>
2016-01-25 00:17:59 -0800

    TrackEditor:  added spline and spline functions for train to follow

--- Assets/TrackEditor/TrackEditor/BezierCurveInspector.cs
+++ Assets/TrackEditor/TrackEditor/BezierCurveInspector.cs
@@ -9,6 +9,8 @@ public class BezierCurveInspector : Editor {
 
     private const int lineSteps = 10;
 
+    private const float directionScale = 0.5f;
+
     private void OnSceneGUI() {
         curve = target as BezierCurve;
         handleTransform = curve.transform;
@@ -23,20 +25,23 @@ public class BezierCurveInspector : Editor {
         Handles.DrawLine(p0, p1);
         Handles.DrawLine(p2, p3);
 
-        Handles.color = Color.white;
-        Vector3 lineStart = curve.GetPoint(0f);
-        Handles.color = Color.green;
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+        
+    }
 
-        for (int i = 0; i <= lineSteps; i++) {
-            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-            Handles.color = Color.white;
-            Handles.DrawLine(lineStart, lineEnd);
-            Handles.color = Color.green;
-            Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
-            lineStart = lineEnd;
+    private void ShowDirections()
+    {
+        Handles.color = Color.green;
+        Vector3 point = curve.GetPoint(0f);
+        Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
+        for (int i = 1; i <= lineSteps; i++)
+        {
+            point = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
         }
     }
-    
+
     private Vector3 ShowPoint(int index) {
         Vector3 point = handleTransform.TransformPoint(curve.points[index]);
         EditorGUI.BeginChangeCheck();
